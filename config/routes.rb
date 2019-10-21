4f64a4f9721c5ecf58104d1586a53b693066a357
3430 <3430@example.com>
2019-10-21 10:58:02 +0900

    routes.rbの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :notecomments
   resources :notes
-=======
   resources :answers
   resources :questions
->>>>>>> 21aa417b0fca7ce8bef53a15ac7300e86c7f6974
   resources :users
   resources :classrooms
   resources :subjects
   resources :grades
   resources :schools
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
