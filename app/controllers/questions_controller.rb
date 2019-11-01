91b99098e5dfe8d9ea52c7868823db2d68a96a6a
3430 <3430@example.com>
2019-11-01 14:52:41 +0900

    controller変更 updateのところ 変更点にミスがあり、元に戻した（まだエラーあり）

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -48,7 +48,6 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
   def update
-    @question = Question.update(question_params)
 
     if params[:question][:picture].present?
       @question.picture = params[:question][:picture].original_filename
