73253cf02e4b545a30325e94feed224c8f08a18b
takuyataki5 <3412@example.com>
2020-01-22 14:59:14 +0900

    チャット　修正後

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,9 +1,9 @@
 class ChatController < ApplicationController
   def show
-    @message = Message.all
     @group_id = params[:group_id]
     @user_id = current_user.id
-    @user_name = current_user.name
+    @user_icon = current_user.icon
+    @messages = Message.where(group_id: @group_id)
   end
  
   def new_message
