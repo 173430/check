e2245d469de7716be841ac4729ece4660d6460dd
3430 <3430@example.com>
2020-01-22 15:01:33 +0900

    noteに検索を付けた

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,15 @@ class Note < ApplicationRecord
 
     has_many :notepictures
 
+    scope :search, -> (search_params) do
+        return if search_params.blank?
+
+        search_like(search_params[:search])
+          .subject_id_is(search_params[:subject_id])
+          .grade_id_is(search_params[:grade_id])
+    end
+    scope :search_like, -> (search){ where('note LIKE ?',"%#{search}%") if search.present? }
+    scope :subject_id_is, -> (subject_id) { where(subject_id: subject_id) if subject_id.present? }
+    scope :grade_id_is, -> (grade_id) { where(grade_id: grade_id) if grade_id.present? }
+
 end
